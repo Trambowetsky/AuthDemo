42b881ae6b37adb104dd017d949c4e8f99603d92
Trambowetsky <deprydepp@gmail.com>
2025-11-27 21:14:10 +0400

    added custom register form

--- AuthDemo/obj/Debug/net8.0/AuthDemo.AssemblyInfo.cs
+++ AuthDemo/obj/Debug/net8.0/AuthDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c9c15229fe97a62cfb43d5f16e0264b6a1abf1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47f93afac8a33ffefc4b7980c72b01ea66cd6f09")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
