ec1f43ddb55b06f4d5952b999914bd06ee09f49a
Trambowetsky <deprydepp@gmail.com>
2025-11-27 21:53:24 +0400

    accessdenied custom page added

--- AuthDemo/obj/Debug/net8.0/AuthDemo.AssemblyInfo.cs
+++ AuthDemo/obj/Debug/net8.0/AuthDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47f93afac8a33ffefc4b7980c72b01ea66cd6f09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42b881ae6b37adb104dd017d949c4e8f99603d92")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
